bb4d5015e576c6e40fbff4aa0030297da4d49a2e
ewhaqh9909 <43231975+ewhaqh9909@users.noreply.github.com>
2018-12-08 15:07:26 +0900

    19

--- main.c
+++ main.c
@@ -21,30 +21,30 @@ int main(int argc, char *argv[]) {
 
 	printf("Reading the data files...\n\n");//1. reading the movie.dat-----------------------------
 	
-	fp=fopen("movie.dat","r");//1.1 FILE open
-	if(fp==NULL)
+	fp=fopen("movie.dat","r");//1.1 FILE open movie.dat을 읽는 형식으로 열기  
+	if(fp==NULL)//파일일 NULL일떄 에러 처리하기  
 	{
 		printf("\n열수 없습니다.\n");
 		exit(1);
 	}
     
 	//1.2 list generation (use function list_genList() )
-	list = list_genList();
+	list = list_genList();//mvInfo의 정보를 연결해주기 위한 초기의 list란 메모리 할당 장소를 열기  
 
 	//1.3 read each movie data from the file and add it to the linked list
 	
 	while (!feof(fp)/* read name, country, runtime and score*/ )
 	{
-		fscanf(fp,"%s %s %d %f",name,country,&runTime,&score);
-		mvInfo=mv_genMvInfo(name, score, runTime, country);
+		fscanf(fp,"%s %s %d %f",name,country,&runTime,&score);//문자열로 읽어준다. 
+		mvInfo=mv_genMvInfo(name, score, runTime, country);//mv_genMvInfo를 MvInfo으로 저장... 
 		//generate a movie info instance(mvInfo) with function mv_genMvInfo()
-		list_addTail(mvInfo, list);
+		list_addTail(mvInfo, list);//list에 mvInfo를 연결해준다. 
         printf("%s %s %d %f\n",name,country,runTime,score);
         
 	}
 
 	fclose(fp);//1.4 FILE close
-	printf("    -totally %d movies are listed!\n",list_len(list));
+	printf("    -totally %d movies are listed!\n",list_len(list));//파일속 영화정보 개수...list_len은 linked_list에 있는 함수로 list안의 노드를 세준다. 
 	//2. program start
 	
 	while(exit_flag == 0)
@@ -59,25 +59,22 @@ int main(int argc, char *argv[]) {
 		printf("-- select an option :");
 		scanf("%d", &option);
 		//2.1 print menu message and get input option
-	
+		
 		switch(option)
 		{
 			case 1: //print all the movies
 				printf("\nprinting all the movies in the list.....\n");
 				printf("----------------------------------------\n");
-				repFunc = mv_printAll;
-				arg = NULL;
-				list_repeatFunc(repFunc,arg,list);	
+				repFunc = mv_printAll;//mv_printALL함수를 repFunc처럼 각 노드마다 반복하기 위해 repFunc로 저장... 
+				arg = NULL;//repFunc의 인수로 쓰이는 arg를 NULL로 두어 별 다른 조건없이 영화전부를 전부 출력하기위한 설정이다  
+				list_repeatFunc(repFunc,arg,list);//노드의 끝까지인 영화정보전체를 다 돌때까지 반복한다  
 				break;
 				
 			case 2: //print movies of specific country
 				printf("select a country :");
-	            scanf("%s", country);
-				arg = &country;
-				cnt++;
-				repFunc = mv_printCountry;
-				list_repeatFunc(repFunc,arg,list);	
-				printf("   - totally %d movies are listed!\n",cnt);
+	            scanf("%s", country);//영화 국가를 입력한다  
+	            arg = &country;//arg를 country로 두어서 repFunc인 mv_printcountry함수에서 활용하게 된다  
+				repFunc = mv_printCountry;//repFunc인 mv_printcountry함수
 				break;
 				
 			case 3: //print movies with long runtime
@@ -85,7 +82,6 @@ int main(int argc, char *argv[]) {
 				printf("lowest runtime :");
 				scanf("%d", &runTime);
 				arg = runTime;
-				list_repeatFunc(repFunc,arg,list);
 				break;
 				
 			case 4: //print movies with high score
@@ -93,7 +89,6 @@ int main(int argc, char *argv[]) {
 				printf("lowest score :");
 				scanf("%f",&score);
 				arg = &score;
-				list_repeatFunc(repFunc,arg,list);
 				break;
 				
 			case 5:
@@ -105,7 +100,7 @@ int main(int argc, char *argv[]) {
 				printf("wrong command! input again\n");
 				break;
 		}
-		
+		printf("   - totally %d movies are listed!\n",list_repeatFunc(repFunc,arg,list));
 		//2.2 printing operation by function pointer (list_repeatFunc() is called here)
 		//2.3 print number of movies
 	}
--- movie.c
+++ movie.c
@@ -18,19 +18,19 @@ void* mv_genMvInfo(char* name, float score, int runTime, char* country)
 	mvPtr = (movInfo_t*)malloc(sizeof(movInfo_t));
 	if (mvPtr != NULL)
 	{
-		strcpy(mvPtr->name,name);
-		mvPtr->score=score;
-		mvPtr->runTime=runTime;
-		strcpy(mvPtr->madeIn,country);
+		strcpy(mvPtr->name,name);//문자열  복사  
+		mvPtr->score=score; //score를 구조체형 score로 저장   
+		mvPtr->runTime=runTime;//runTime를 구조체형 runTime로 저장  
+		strcpy(mvPtr->madeIn,country);//문자열 복사  
 	}
 	
 	return (void*)mvPtr;	
-}
+}//name,score,runTime,country를 구조체에 저장공간을 할당해준것에 저장한다  
 
 void printMv(void* obj)
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
-	if (mvPtr == NULL)
+	if (mvPtr == NULL) //구조 
 	{
 		printf("[ERROR] failed to print the movie Info! (object is NULL)\n");
 	}
@@ -50,33 +50,35 @@ int mv_printAll(void* obj, void* arg)
 int mv_printScore(void* obj, void* arg)
 {
 movInfo_t* mvPtr = (movInfo_t*)obj;
-	if ( mvPtr->score >= *(float*)arg) 
+	if ( mvPtr->score >= *(float*)arg ) 
 	{
 		printMv(obj);
+		return 1;
 	}
 
-	return;
+	return 1;
 }
 
 int mv_printRunTime(void* obj, void* arg)
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
-	if ( mvPtr->runTime >= arg) 
+	if ( (mvPtr->runTime) >=  *(float*)arg) 
 	{
 		printMv(obj);
+		return 1;
 	}
-	return;
+	return 0;
 }
 
 int mv_printCountry(void* obj, void* arg)
 {	
-	movInfo_t* mvPtr = (movInfo_t*)obj;
-	if(strcmp(arg,mvPtr->madeIn)==0)
+	movInfo_t* mvPtr = (movInfo_t*)obj;//매개변수였던 obj를 구조체형의 mvPtr로 사용  
+	if(strcmp(arg,mvPtr->madeIn)==0)//문자열 arg와  mvPtr->madeIn를 비교하여 같을때 printMv로 그 노드를 출력한다. 
 	{
 		printMv(obj);
-	}
-
-	return ;
+		return 1;
+	}else 
+	return 0;
 }
 
 
